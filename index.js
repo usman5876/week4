dcf233936f82b0b6abe166c9474d209bff537f76
Muhammad Usman <60290215+usman5876@users.noreply.github.com>
2023-08-06 11:54:10 +0500

    new file:   index.js 	new file:   index.ts 	new file:   package.json 	new file:   tsconfig.json

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+// let width:number = 23;
+// let hight:number = 43;
+// let area = width * hight;
+// console.log(area);
+// let width:number = 5;
+// let hight:number = 3;
+// let depth:number = 5;
+// let volume = width * hight * depth ;
+// console.log(volume);
+// let width:number = 0;
+// if(width > 0){
+//     console.log("Number is positive");
+// }
+// else if(width < 0){
+//     console.log("Number is negative");
+// }
+// else{
+//     console.log("Number is zero");
+// }
+// let value:number = 5;
+// if (value%2 == 0) {
+//     console.log("number is even");
+// } else {
+//     console.log("value is odd");
+// }
+// let age:number = 20;
+//  if (age>18) {
+//      console.log("Perspn is eligible for vote");
+//  } else {
+//      console.log("person is not eligble for votting");
+//  }
+var calc = ((10 + 5) * 3 - 2) / ((4 % 3) - 7);
+console.log(calc);
